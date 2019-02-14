0ab59e5fdb633fc4c0e7bb7918fc768ce33bb279
Peter Pae <paendabear@gmail.com>
2019-02-14 13:08:58 -0500

    second commit becuase i forgot to save

--- practice.js
+++ practice.js
@@ -1,9 +1,13 @@
-function avg (numArr) {
+var numArr = [,2,3,4,5,6];
+
+avg ((numArr) => {
     let total = 0;
 for (let i=0; i<numArr.length; i++){
     total += numArr[i];
 
     return (total/(numArr.length));
-}
-}
+})
 
+numArr.reduce( (total, num) => {
+    return total + num
+})
