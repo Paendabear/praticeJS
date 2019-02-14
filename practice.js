ed7028c96ae41db011a546205be3bbfea947c669
Peter Pae <paendabear@gmail.com>
2019-02-14 13:22:18 -0500

    third question answer

--- practice.js
+++ practice.js
@@ -1,16 +1,21 @@
 var numArr = [,2,3,4,5,6];
 
-avg ((numArr) => {
-    let total = 0;
-for (let i=0; i<numArr.length; i++){
-    total += numArr[i];
+// avg ((numArr) => {
+//     let total = 0;
+// for (let i=0; i<numArr.length; i++){
+//     total += numArr[i];
 
-    return (total/(numArr.length));
-}
-});
-
+//     return (total/(numArr.length));
+// }
+// });
 
 
 numArr.reduce( (total, num) => {
     return total + num
 })
+
+let people = [{age: 21},{age: 22},{age: 23},];
+
+const doubled = people.map((key) => {
+    people.key*= 2;
+});
